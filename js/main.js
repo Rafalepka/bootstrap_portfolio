d76c879a9224b2dc15682b3d40bfae33d48d7c06
Rafal Matalowski <rafal.matalowski@gmail.com>
2016-12-18 16:53:00 +0100

    final version

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(function () {
+    
+window.sr = ScrollReveal({ reset: true }).reveal('.container, .row');
+    
+    if ($(window).scrollTop() >= 50) {
+
+        $('#main_nav').addClass('scroll')
+
+
+    } else {
+        $('#main_nav').removeClass('scroll')
+
+    }
+$(window).scroll(function () {
+
+    if ($(window).scrollTop() >= 50) {
+
+        $('#main_nav').addClass('scroll')
+
+
+    } else {
+        $('#main_nav').removeClass('scroll')
+
+    }
+});
+    
+var root = $('html, body');
+$('a[href^="#"]').click(function () {
+    var href = $(this).attr('href');
+    root.animate({
+        scrollTop: $(href).offset().top - 52
+    }, 500);
+
+});
+
+
+});
